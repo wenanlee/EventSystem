1c8576f7ace652660aff111617590ad134913c48
Wenan <Wenan@DESKTOP-5JN67NN>
2019-06-21 10:12:34 +0800

    ok

---
+++ Assets/EFramework/Core/Event/Event/EventData.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EventData:EventBase {
+    public object[] args=null;
+    public EventData(Enum eid){
+        this.eid = eid;
+    }
+    public EventData(Enum eid,params object[] args)
+    {
+        this.eid = eid;
+        this.args = args;
+    }
+    public static EventData CreateEvent(Enum e)
+    {
+        EventData eventBase = new EventData(e);
+        return eventBase;
+    }
+    public static EventData CreateEvent(Enum e,params object[] args)
+    {
+        EventData eventBase = new EventData(e, args);
+        return eventBase;
+    }
+}
+public class EventData<T> : EventBase
+{
+    public T[] args = null;
+    public EventData(Enum eid)
+    {
+        this.eid = eid;
+    }
+    public EventData(Enum eid, params T[] args)
+    {
+        this.eid = eid;
+        this.args = args;
+    }
+    public static EventData CreateEvent(Enum e)
+    {
+        EventData eventBase = new EventData(e);
+        return eventBase;
+    }
+    public static EventData CreateEvent(Enum e, params object[] args)
+    {
+        EventData eventBase = new EventData(e, args);
+        return eventBase;
+    }
+}
+public class EventBase
+{
+    public Enum eid;
+    public void Send()
+    {
+        if (EventManager.Instance != null) EventManager.Instance.SendEvent(this);
+    }
+}
